f331c7789bfe1e21f75b729cf082aca3de213a4c
dracoDevs <Mickiel.araya@gmail.com>
2025-10-21 17:44:29 -0400

    Update client.go

--- client.go
+++ client.go
@@ -121,6 +121,7 @@ func (c *Client) configureAfterLogin() error {
 
 func (c *Client) loadLoginPage() *ModuleParser {
 	moduleLoader := &ModuleParser{client: c}
+	fmt.Println(c.getEndpoint("login_page"))
 	moduleLoader.Load(c.getEndpoint("login_page"))
 	return moduleLoader
 }
